c71756afc84ba997df1d89a2319a29160f15d50b
OussamaAtifii <oussamaati03@gmail.com>
2024-04-10 23:59:21 +0200

    Add global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Añadir pipe de validación global
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: ['http://localhost:5173'],
     methods: ['GET', 'POST'],
     credentials: true,
   });
+
   await app.listen(3000);
 }
 bootstrap();
