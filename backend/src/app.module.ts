30b5857cb6dbd35ff39e2260294be6d13672a24a
OussamaAtifii <oussamaati03@gmail.com>
2024-04-12 00:06:08 +0200

    Add BrandsModule to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     AuthModule,
+    BrandsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
