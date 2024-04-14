5bcfc96cf99a9d3f686df68c898ff2f5ca2b3052
OussamaAtifii <oussamaati03@gmail.com>
2024-04-14 18:39:22 +0200

    Fix import statement in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ErrorInterceptor } from './interceptors/error/error.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
