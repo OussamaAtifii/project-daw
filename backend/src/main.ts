d880ab918c45ab519653772f2c3c0bd99866f5e9
OussamaAtifii <oussamaati03@gmail.com>
2024-04-14 15:53:10 +0200

    Add ErrorInterceptor globally

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ErrorInterceptor } from './interceptors/error/error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Añadir pipe de validación global
+  // Añadir de forma global el pipe de validación
   app.useGlobalPipes(new ValidationPipe());
+
+  // Añadir de forma global el interceptor de los errores
+  app.useGlobalInterceptors(new ErrorInterceptor());
+
   app.enableCors({
     origin: ['http://localhost:5173'],
     methods: ['GET', 'POST'],
