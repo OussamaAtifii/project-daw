01ed935943777b78bc6cec5ea8b077b59e81dc6a
OussamaAtifii <oussamaati03@gmail.com>
2024-04-17 20:59:30 +0200

    Add ServeStaticModule for serving static files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,11 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { BrandsModule } from './brands/brands.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     AuthModule,
     BrandsModule,
