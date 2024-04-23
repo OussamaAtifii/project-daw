ad396f616fb320f22022019d920a63c467256d63
OussamaAtifii <oussamaati03@gmail.com>
2024-04-23 18:39:17 +0200

    Add CartIcon and SearchIcon components

---
+++ frontend/src/components/Icons.jsx
@@ -0,0 +1,20 @@
+export function CartIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width={24} height={24} color='#000000' fill='none'>
+      <path d='M8 16H15.2632C19.7508 16 20.4333 13.1808 21.261 9.06908C21.4998 7.88311 21.6192 7.29013 21.3321 6.89507C21.045 6.5 20.4947 6.5 19.3941 6.5H6' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M8 16L5.37873 3.51493C5.15615 2.62459 4.35618 2 3.43845 2H2.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M8.88 16H8.46857C7.10522 16 6 17.1513 6 18.5714C6 18.8081 6.1842 19 6.41143 19H17.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+      <circle cx='10.5' cy='20.5' r='1.5' stroke='currentColor' strokeWidth='1.5' />
+      <circle cx='17.5' cy='20.5' r='1.5' stroke='currentColor' strokeWidth='1.5' />
+    </svg>
+  )
+}
+
+export function SearchIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width={20} height={20} color='#000000' fill='none'>
+      <path d='M17.5 17.5L22 22' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z' stroke='currentColor' strokeWidth='1.5' strokeLinejoin='round' />
+    </svg>
+  )
+}
