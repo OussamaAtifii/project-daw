0092d43144a8d8e9207160ff9f1f157584d193fe
OussamaAtifii <oussamaati03@gmail.com>
2024-04-25 23:26:32 +0200

    Update icon sizes and add new icons

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -1,6 +1,6 @@
 export function CartIcon () {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width={24} height={24} color='#000000' fill='none'>
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width={28} height={28} color='#000000' fill='none'>
       <path d='M8 16H15.2632C19.7508 16 20.4333 13.1808 21.261 9.06908C21.4998 7.88311 21.6192 7.29013 21.3321 6.89507C21.045 6.5 20.4947 6.5 19.3941 6.5H6' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
       <path d='M8 16L5.37873 3.51493C5.15615 2.62459 4.35618 2 3.43845 2H2.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
       <path d='M8.88 16H8.46857C7.10522 16 6 17.1513 6 18.5714C6 18.8081 6.1842 19 6.41143 19H17.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
@@ -15,6 +15,67 @@ export function SearchIcon () {
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width={20} height={20} color='#000000' fill='none'>
       <path d='M17.5 17.5L22 22' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
       <path d='M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z' stroke='currentColor' strokeWidth='1.5' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function AddCartIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24' color='#000000' fill='none'>
+      <path d='M8 16.0001H15.2632C19.7508 16.0001 20.4333 13.1809 21.261 9.06916C21.4998 7.8832 21.6192 7.29022 21.3321 6.89515C21.1034 6.58048 20.7077 6.51645 20 6.50342' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M9 6.5H17M13 10.5V2.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M8 16L5.37873 3.51493C5.15615 2.62459 4.35618 2 3.43845 2H2.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M8.88 16H8.46857C7.10522 16 6 17.1513 6 18.5714C6 18.8081 6.1842 19 6.41143 19H17.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+      <circle cx='10.5' cy='20.5' r='1.5' stroke='currentColor' strokeWidth='1.5' />
+      <circle cx='17.5' cy='20.5' r='1.5' stroke='currentColor' strokeWidth='1.5' />
+    </svg>
+  )
+}
+
+export function RemoveCartIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24' color='#000000' fill='none'>
+      <path d='M8 16L16.7201 15.2733C19.4486 15.046 20.0611 14.45 20.3635 11.7289L21 6' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M6 6H8M22 6H18.5' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M10.5 3L13.5 6M13.5 6L16.5 9M13.5 6L10.5 9M13.5 6L16.5 3' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <circle cx='6' cy='20' r='2' stroke='currentColor' strokeWidth='1.5' />
+      <circle cx='17' cy='20' r='2' stroke='currentColor' strokeWidth='1.5' />
+      <path d='M8 20L15 20' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+      <path d='M2 2H2.966C3.91068 2 4.73414 2.62459 4.96326 3.51493L7.93852 15.0765C8.08887 15.6608 7.9602 16.2797 7.58824 16.7616L6.63213 18' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+    </svg>
+  )
+}
+
+export function DeliveryIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='20' height='20' color='#36cc09' fill='none'>
+      <path d='M3 13.3333C3 13.3333 4.5 14 6.5 17C6.5 17 6.78485 16.5192 7.32133 15.7526M17 6C14.7085 7.14577 12.3119 9.55181 10.3879 11.8223' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+      <path d='M8 13.3333C8 13.3333 9.5 14 11.5 17C11.5 17 17 8.5 22 6' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function UserIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='28' height='28' color='#000000' fill='none'>
+      <path d='M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z' stroke='currentColor' strokeWidth='1.5' />
+      <path d='M7.5 17C9.8317 14.5578 14.1432 14.4428 16.5 17M14.4951 9.5C14.4951 10.8807 13.3742 12 11.9915 12C10.6089 12 9.48797 10.8807 9.48797 9.5C9.48797 8.11929 10.6089 7 11.9915 7C13.3742 7 14.4951 8.11929 14.4951 9.5Z' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' />
+    </svg>
+  )
+}
+
+export function ArrowRightIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='32' height='32' color='#000000' fill='none'>
+      <path d='M9.00005 6C9.00005 6 15 10.4189 15 12C15 13.5812 9 18 9 18' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
+    </svg>
+  )
+}
+
+export function ArrowLeftIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='32' height='32' color='#000000' fill='none'>
+      <path d='M15 6C15 6 9.00001 10.4189 9 12C8.99999 13.5812 15 18 15 18' stroke='currentColor' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
     </svg>
   )
 }
