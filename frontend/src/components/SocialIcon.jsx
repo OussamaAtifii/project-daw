f148d598322784d437b9cd95cbc9a911fa47c601
OussamaAtifii <oussamaati03@gmail.com>
2024-04-27 04:03:08 +0200

    Add SocialIcon component

---
+++ frontend/src/components/SocialIcon.jsx
@@ -0,0 +1,7 @@
+export default function SocialIcon ({ icon }) {
+  return (
+    <div className='rounded-full bg-black w-9 h-9 flex justify-center items-center hover:bg-gray-900'>
+      {icon}
+    </div>
+  )
+}
