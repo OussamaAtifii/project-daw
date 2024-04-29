ead2bb2c6bbb4e93f5c3f0d93b1c0bf6c155f54b
OussamaAtifii <oussamaati03@gmail.com>
2024-04-29 18:01:27 +0200

    Update CategoryCard styling and image size

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -2,15 +2,15 @@ import { getImageUrl } from '../utils/utils'
 
 export default function CategoryCard ({ category }) {
   return (
-    <div className='border border-gray-300 rounded-lg flex gap-4 p-4 items-center shadow-md hover:shadow-lg hover:bg-gray-100 transition-all cursor-pointer'>
+    <div className='border border-gray-300 rounded-lg flex text-black hover:text-main-900 gap-4 pl-3 items-center shadow-md hover:shadow-lg hover:bg-gray-100 transition-all cursor-pointer'>
       <div className='flex-shrink-0'>
         <img
           src={getImageUrl(category.image)}
-          className='h-20 w-20 object-cover rounded-md'
+          className='h-24 w-24 object-cover rounded-md'
           alt={category.name}
         />
       </div>
-      <p className='font-bold text-lg text-gray-800'>{category.name}</p>
+      <p className='font-bold text-lg'>{category.name}</p>
     </div>
   )
 }
