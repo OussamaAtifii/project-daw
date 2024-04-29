8cbb73bd42b6aaa7754e8644f677e5f81bbc2622
OussamaAtifii <oussamaati03@gmail.com>
2024-04-29 18:01:50 +0200

    Add categories and sliders to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,56 @@
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import CategoryCard from '../components/CategoryCard'
+import SliderAuto from '../components/SliderAuto'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import NoveltiesSlider from '../components/NoveltiesSlider'
+
+import { getCategories } from '../services/api'
 
 function HomePage () {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const data = await getCategories()
+      setCategories(data)
+    }
+
+    fetchCategories()
+  }, [])
+
   return (
     <>
-      <nav className='bg-white border-b-2 border-main-100'>
-        <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <Link to='/'>
-            <span className='text-2xl font-semibold whitespace-nowrap'>TecnoNexo</span>
-          </Link>
-          <div className='flex gap-4'>
-            <Link to='/login'>
-              <button className='bg-main bg-main-500 px-4 py-2 rounded-md text-white '>Iniciar sesión</button>
-            </Link>
+      <Navbar />
+      <main className='max-w-screen-xl mx-auto'>
+        <section className='flex gap-4 mb-8'>
+          <div>
+            <SliderAuto />
           </div>
-        </div>
-      </nav>
+          <div className='flex flex-col gap-2 w-full'>
+            <div className='border border-gray-300 rounded-lg flex gap-4 p-4 items-center shadow-md hover:shadow-lg hover:bg-gray-100 transition-all cursor-pointer h-1/2'>
+              {/* TODO */}
+            </div>
+            <div className='border border-gray-300 rounded-lg flex gap-4 p-4 items-center shadow-md hover:shadow-lg hover:bg-gray-100 transition-all cursor-pointer h-1/2'>
+              {/* TODO */}
+            </div>
+          </div>
+        </section>
+        <h2 className='text-center text-2xl font-bold mb-8'>Explora nuestras categorías</h2>
+        {/* Seccion para mostrar todas las categorias */}
+        <section className='grid grid-cols-3 gap-6 mb-14'>
+          {categories.map(category => (
+            <Link key={category.id} to={`/categories/${category.id}`}>
+              <CategoryCard category={category} />
+            </Link>
+          ))}
+        </section>
+      </main>
+      <section className='h-96 bg-gray-200 w-full p-11'>
+        <NoveltiesSlider />
+      </section>
+      <Footer />
     </>
   )
 }
