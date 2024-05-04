6cf9c119be5b5a45e39e8d9f24e5f2b2d800b2da
OussamaAtifii <oussamaati03@gmail.com>
2024-05-04 10:32:05 +0200

    Add OrdersModule to app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { ProductsModule } from './products/products.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { StripeModule } from './stripe/stripe.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { StripeModule } from './stripe/stripe.module';
     CategoriesModule,
     ProductsModule,
     StripeModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
