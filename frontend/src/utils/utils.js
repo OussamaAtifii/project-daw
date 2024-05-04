2dad7fe6871e02e4019ce366562f79a96f29fce5
OussamaAtifii <oussamaati03@gmail.com>
2024-05-04 10:33:45 +0200

    Add formatDate function to format date strings

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -9,3 +9,24 @@ const IMAGES_URL = 'http://localhost:3000/images/'
 export function getImageUrl (image) {
   return `${IMAGES_URL}${image}`
 }
+
+/**
+ * Formatea una cadena de fecha dada en el formato dd/mm/yyyy.
+ *
+ * @param {string} dateString - La cadena de fecha a formatear.
+ * @returns {string} La cadena de fecha formateada en el formato dd/mm/yyyy.
+ */
+export function formatDate (dateString) {
+  const date = new Date(dateString)
+  // Obtener el día y asegurar que tenga dos dígitos
+  const day = date.getDate().toString().padStart(2, '0')
+
+  // Obtener el mes (se suma 1 porque en JavaScript los meses van de 0 a 11) y asegurar que tenga dos dígitos
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+
+  // Obtener el año
+  const year = date.getFullYear()
+
+  // Retornar la fecha formateada en el formato dd/mm/yyyy
+  return `${day}/${month}/${year}`
+}
