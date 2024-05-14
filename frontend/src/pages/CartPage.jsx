3f61446afb01237e4cb5d8a591403fc1730f84d5
OussamaAtifii <oussamaati03@gmail.com>
2024-05-14 21:51:18 +0200

    Add link to checkout page and update cart subtotal calculation

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -2,17 +2,25 @@ import { useMemo } from 'react'
 import CheckoutProductCard from '../components/CheckoutProductCard'
 import Navbar from '../components/Navbar'
 import { useCart } from '../hooks/useCart'
+import { Link } from 'react-router-dom'
+import { RemoveCartIcon } from '../components/Icons'
+import Breadcrumbs from '../components/Breadcrumbs'
 
 export default function CartPage () {
   const { cart } = useCart()
 
   const subtotal = useMemo(() => {
-    return cart.reduce((acc, product) => acc + product.price, 0)
+    const totalPrice = cart.reduce(
+      (acc, product) => acc + product.price * product.quantity,
+      0
+    )
+    return totalPrice.toFixed(2)
   }, [cart])
 
   return (
     <>
       <Navbar />
+      <Breadcrumbs />
       {cart.length > 0
         ? (
           <div className='max-w-screen-xl mx-auto flex gap-4'>
@@ -45,16 +53,28 @@ export default function CartPage () {
                 </div>
                 {/* Botón de realizar pedido */}
                 <div className='pt-6'>
-                  <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full'>
-                    Realizar pedido
-                  </button>
+                  <Link to='/checkout'>
+                    <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full'>
+                      Realizar pedido
+                    </button>
+                  </Link>
                 </div>
               </div>
             </section>
           </div>
           )
         : (
-          <p className='text-center '>HOLA</p>
+          <div className='flex flex-col items-center gap-4'>
+            <div className='bg-main-200 rounded-full p-4 '>
+              <RemoveCartIcon />
+            </div>
+            <p>
+              Tu carrito está vacío.{' '}
+              <Link to='/' className='text-main-600 italic hover:underline'>
+                ¡Explora nuestros productos!
+              </Link>
+            </p>
+          </div>
           )}
     </>
   )
