bbeb5c0fc92b026ac1e7fe58e01d9cef3a5b4635
OussamaAtifii <oussamaati03@gmail.com>
2024-05-15 23:12:15 +0200

    Remove minimum length validation for password in login

--- backend/src/auth/dto/login-auth.dto.ts
+++ backend/src/auth/dto/login-auth.dto.ts
@@ -1,11 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginAuthDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @MinLength(8)
   @IsNotEmpty()
   @IsString()
   password: string;
