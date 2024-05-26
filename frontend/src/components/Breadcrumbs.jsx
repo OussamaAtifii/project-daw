e0c5c492b9ad4976bb2e60a970aae74e2adb43b0
OussamaAtifii <oussamaati03@gmail.com>
2024-05-26 23:17:25 +0200

    Update Breadcrumbs component to improve layout and truncate long names

--- frontend/src/components/Breadcrumbs.jsx
+++ frontend/src/components/Breadcrumbs.jsx
@@ -1,7 +1,7 @@
 import { useMatches } from 'react-router-dom'
 import { ArrowRightIcon } from './Icons'
 
-export default function Breadcrumbs ({ name = '' }) { // Aquí usamos destructuración para obtener la prop 'name'
+export default function Breadcrumbs ({ name = '' }) {
   const matches = useMatches()
   const crumbs = matches
     // Filtramos los matches que no tengan handle y crumb
@@ -10,12 +10,11 @@ export default function Breadcrumbs ({ name = '' }) { // Aquí usamos destructur
     .map((match) => match.handle.crumb(match.data))
 
   return (
-    <ol className='flex items-center space-x-1 md:space-x-2 max-w-screen-xl mx-auto'>
-      {/* Aquí puedes usar 'name' si necesitas, por ejemplo en un encabezado */}
+    <ol className='flex items-center space-x-1 md:space-x-2 max-w-screen-xl mx-2 xl:mx-auto mb-6 truncate overflow-ellipsis'>
       {crumbs.map((crumb, index) => {
         if (index === crumbs.length - 1 && name) {
           return (
-            <li key={index} className=' flex items-center gap-2 text-sm font-medium text-gray-700'>{crumb} <ArrowRightIcon size={20} /> {name}</li>
+            <li key={index} className='flex items-center gap-2 text-sm font-medium text-gray-700 '>{crumb} <ArrowRightIcon size={20} /> {name}</li>
           )
         }
         return <li key={index} className='flex'>{crumb}</li>
