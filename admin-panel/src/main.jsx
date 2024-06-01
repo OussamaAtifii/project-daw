c13a3727d36a9cd277f6f3e8cca6ae162afb60cb
OussamaAtifii <oussamaati03@gmail.com>
2024-06-01 22:13:53 +0200

    add react-router

--- admin-panel/src/main.jsx
+++ admin-panel/src/main.jsx
@@ -3,12 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './components/ThemeProvider.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import UpdateCategory from './categories/UpdateCategory.jsx'
+import CreateCategory from './categories/CreateCategory.jsx'
+import { Toaster } from './components/ui/toaster.jsx'
+import CreateBrand from './brands/CreateBrand.jsx'
+import UpdateBrand from './brands/UpdateBrand.jsx'
+import UpdateUser from './users/UpdateUser.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/category/create',
+    element: <CreateCategory />
+  },
+  {
+    path: '/update-category/:id',
+    element: <UpdateCategory />
+  },
+  {
+    path: '/brand/create',
+    element: <CreateBrand />
+  },
+  {
+    path: '/update-brand/:id',
+    element: <UpdateBrand />
+  },
+  {
+    path: '/update-user/:id',
+    element: <UpdateUser />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <RouterProvider router={router} />
+    <Toaster />
   </ThemeProvider>
-
 )
